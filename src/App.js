132ac158e364fdb061f7c89de3a99df715f97410
Melih Berkay <maydin13@ku.edu.tr>
2021-09-19 15:46:47 +0300

    Deployment files and changes

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ import AddNote from "./components/Notes/AddNote";
 import NoteContext from "./store/note-context";
 
 function App() {
-  // const [notes, setNotes] = useState([
-  //   { id: nanoid(), text: "This is my first note!", date: "15/04/2021" },
-  //   { id: nanoid(), text: "This is my second note!", date: "16/04/2021" },
-  //   { id: nanoid(), text: "This is my third note!", date: "17/04/2021" },
-  //   { id: nanoid(), text: "This is my fourth note!", date: "18/04/2021" },
-  // ]);
   const [filterText, setFilterText] = useState("");
   const [darkMode, setDarkMode] = useState(true);
 
@@ -36,24 +30,6 @@ function App() {
     setDarkMode((prevMode) => !prevMode);
   };
 
-  // const addNote = (text) => {
-  //   const date = new Date();
-  //   const newNote = {
-  //     id: nanoid(),
-  //     text: text,
-  //     date: date.toLocaleDateString(),
-  //   };
-
-  //   setNotes((prevNotes) => {
-  //     return [newNote, ...prevNotes];
-  //   });
-  // };
-
-  // const deleteNote = (id) => {
-  //   const remainingNotes = notes.filter((note) => note.id !== id);
-  //   setNotes(remainingNotes);
-  // };
-
   const onFilter = (text) => {
     setFilterText(text);
   };
@@ -63,18 +39,8 @@ function App() {
       <div className="container">
         <Header darkMode={darkMode} onModeChange={changeMode} />
         <Search onFilter={onFilter} />
-        <AddNote
-        //onAdd={addNote}
-        />
-        <NotesList
-          filterText={filterText}
-          darkMode={darkMode}
-          // onDelete={deleteNote}
-          // onAdd={addNote}
-          // notes={notes.filter((note) =>
-          //   note.text.toLocaleLowerCase().includes(filterText)
-          //)}
-        />
+        <AddNote />
+        <NotesList filterText={filterText} darkMode={darkMode} />
       </div>
     </div>
   );
