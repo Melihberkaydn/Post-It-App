30eacce082cac65cf8fa4214004d778207056d73
Melih Berkay Aydın <maydin13@ku.edu.tr>
2024-03-02 15:11:56 +0300

    Login and Logout Pages are added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Layout";
+import AuthenticationPage, {
+  action as authAction,
+} from "../src/pages/AuthenticationPage";
+import { action as logoutAction } from "./pages/Logout";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
+    },
+    {
+      path: "auth",
+      element: <AuthenticationPage />,
+      action: authAction,
+    },
+    {
+      path: "logout",
+      action: logoutAction,
     },
   ]);
 
